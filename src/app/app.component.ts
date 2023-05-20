1a99b2069a0b427edcc32ee99d2493f1dee0cacb
alejandro <ale.luis@gmail.com>
2023-05-20 16:12:22 -0300

    add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,40 @@ import { ClipboardModule } from '@angular/cdk/clipboard';
   imports: [CommonModule, RouterOutlet, ReactiveFormsModule, ClipboardModule],
   template: `
     <style>
+
       .container {
         display: flex;
         flex-direction: column;
-        width: 400px;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .popup {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
         gap: 20px;
+        margin-top: 50px;
+        margin-bottom: 50px;
         padding: 20px;
         font-family: Arial, Helvetica, sans-serif;
+        background-color: rgba(55, 55, 55, .1);
+        backdrop-filter: blur(5px);
+        border-radius: 10px;
+        width: 600px;
+        height: 60vh;
       }
 
       .alert.alert-danger {
-        color: red;
+        color: rgba(205, 0, 0 , .8);
+        font-size: 12px;
       }
 
-      .cmd-container {
+      .cmd-box {
         display: flex;
         flex-direction: column;
+        justify-content: center;
         width: 90%;
         max-width: 1000px;
         height: 100px;
@@ -34,6 +52,13 @@ import { ClipboardModule } from '@angular/cdk/clipboard';
         background-color: #263238;
         border-radius: 5px;
         box-shadow: 0 5px 30px rgba(100, 0, 200, 0.5);
+      }
+
+      .cmd-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
       }
 
       .cmd-header {
@@ -84,120 +109,145 @@ import { ClipboardModule } from '@angular/cdk/clipboard';
 
       .item-row {
         display: flex;
-        gap: 5px;
         flex-direction: column;
+        width: 400px;
+      }
+
+      .item-row input, select {
+        border: none;
+        padding: 10px;
+        border-radius: 5px;
+        transition: all .3s ease-in-out;
+      }
+
+      .item-row input:focus, select:focus {
+         outline: none;
+          background-color: rgb(193, 226, 255);
       }
 
     </style>
 
     <form [formGroup]="optionsForm" (ngSubmit)="submit()">
       <div class="container">
-        <div class="item-row">
-            <label for="name">Proyect's name* </label>
-            <input id="name" type="text" formControlName="name" placeholder="example">
-            <div *ngIf="this.optionsForm.invalid && (this.name?.dirty || this.name?.touched)" class="alert alert-danger">
-              <div *ngIf="this.name?.errors?.['required']">
-                Name is required.
+        <div class="popup">
+          <div class="item-row">
+              <label for="name">Proyect's name* </label>
+              <input id="name" type="text" autocomplete="off" autofocus formControlName="name" placeholder="example">
+              <div *ngIf="this.optionsForm.invalid && (this.name?.dirty || this.name?.touched)" class="alert alert-danger">
+                <div *ngIf="this.name?.errors?.['required']">
+                  Name is required.
+                </div>
+                <div *ngIf="this.name?.errors?.['minlength']">
+                  Name must be at least 3 characters long.
+                </div>
+                <div *ngIf="this.name?.errors?.['maxlength']">
+                  Name must have less than 30 characters.
+                </div>
               </div>
-              <div *ngIf="this.name?.errors?.['minlength']">
-                Name must be at least 3 characters long.
+              <div class="alert alert-danger" *ngIf="!this.name?.errors || (!this.name?.dirty && !this.name?.touched)">
+                <div>&nbsp;</div>
               </div>
-              <div *ngIf="this.name?.errors?.['maxlength']">
-                Name must have less than 30 characters.
+          </div>
+          <div class="item-row">
+            <label for="package-manager">Package manager* </label>
+            <select id="package-manager" formControlName="packageManager">
+              <option [ngValue]="null">Select</option>
+              <option *ngFor="let option of packagesOptions" [ngValue]="option.id">{{option.value}}</option>
+            </select>
+            <div *ngIf="this.optionsForm.invalid && (this.packageManager?.dirty || this.packageManager?.touched)" class="alert alert-danger">
+                <div *ngIf="this.packageManager?.errors?.['required']">
+                  Package manager is required.
+                </div>
+            </div>
+            <div class="alert alert-danger" *ngIf="!this.packageManager?.errors || (!this.packageManager?.dirty && !this.packageManager?.touched)">
+                <div>&nbsp;</div>
+            </div>
+          </div>
+          <div class="item-row">
+            <label for="style-type">Style type* </label>
+            <select id="style-type" formControlName="style">
+              <option [ngValue]="null">Select</option>
+              <option *ngFor="let option of stylesOptions" [ngValue]="option.id">{{option.value}}</option>
+            </select>
+            <div *ngIf="this.optionsForm.invalid && (this.style?.dirty || this.style?.touched)" class="alert alert-danger">
+              <div *ngIf="this.style?.errors?.['required']">
+                Style type is required.
               </div>
             </div>
-        </div>
-        <div class="item-row">
-          <label for="package-manager">Package manager* </label>
-          <select id="package-manager" formControlName="packageManager">
-            <option [ngValue]="null">Select</option>
-            <option *ngFor="let option of packagesOptions" [ngValue]="option.id">{{option.value}}</option>
-          </select>
-          <div *ngIf="this.optionsForm.invalid && (this.packageManager?.dirty || this.packageManager?.touched)" class="alert alert-danger">
-              <div *ngIf="this.packageManager?.errors?.['required']">
-                Package manager is required.
+            <div class="alert alert-danger" *ngIf="!this.style?.errors || (!this.style?.dirty && !this.style?.touched)">
+                <div>&nbsp;</div>
+            </div>
+          </div>
+          <div class="item-row">
+            <label for="style-inline">Style inline
+              <input id="style-inline" type="checkbox" formControlName="styleInline" />
+            </label>
+            <div *ngIf="this.optionsForm.invalid && (this.styleInline?.dirty || this.styleInline?.touched)" class="alert alert-danger">
+              <div *ngIf="this.styleInline?.errors?.['required']">
+                Style inline is required.
               </div>
             </div>
-        </div>
-        <div class="item-row">
-          <label for="style-type">Style type* </label>
-          <select id="style-type" formControlName="style">
-            <option [ngValue]="null">Select</option>
-            <option *ngFor="let option of stylesOptions" [ngValue]="option.id">{{option.value}}</option>
-          </select>
-          <div *ngIf="this.optionsForm.invalid && (this.style?.dirty || this.style?.touched)" class="alert alert-danger">
-            <div *ngIf="this.style?.errors?.['required']">
-              Style type is required.
+          </div>
+          <div class="item-row">
+            <label for="template-inline">Template inline
+              <input id="template-inline" type="checkbox" formControlName="templateInline" />
+            </label>
+            <div *ngIf="this.optionsForm.invalid && (this.templateInline?.dirty || this.templateInline?.touched)" class="alert alert-danger">
+              <div *ngIf="this.templateInline?.errors?.['required']">
+                Template inline is required.
+              </div>
             </div>
           </div>
-        </div>
-        <div class="item-row">
-          <label for="style-inline">Style inline
-            <input id="style-inline" type="checkbox" formControlName="styleInline" />
-          </label>
-          <div *ngIf="this.optionsForm.invalid && (this.styleInline?.dirty || this.styleInline?.touched)" class="alert alert-danger">
-            <div *ngIf="this.styleInline?.errors?.['required']">
-              Style inline is required.
+          <div class="item-row">
+            <label for="routing">Routing
+              <input id="routing" type="checkbox" formControlName="routing" />
+            </label>
+            <div *ngIf="this.optionsForm.invalid && (this.routing?.dirty || this.routing?.touched)" class="alert alert-danger">
+              <div *ngIf="this.routing?.errors?.['required']">
+                Routing is required.
+              </div>
             </div>
           </div>
-        </div>
-        <div class="item-row">
-          <label for="template-inline">Template inline
-            <input id="template-inline" type="checkbox" formControlName="templateInline" />
-          </label>
-          <div *ngIf="this.optionsForm.invalid && (this.templateInline?.dirty || this.templateInline?.touched)" class="alert alert-danger">
-            <div *ngIf="this.templateInline?.errors?.['required']">
-              Template inline is required.
+          <div class="item-row">
+            <label for="standalone">Standalone
+              <input id="standalone" type="checkbox" formControlName="standalone" />
+            </label>
+            <div *ngIf="this.optionsForm.invalid && (this.standalone?.dirty || this.standalone?.touched)" class="alert alert-danger">
+              <div *ngIf="this.standalone?.errors?.['required']">
+                Standalone is required.
+              </div>
             </div>
           </div>
-        </div>
-        <div class="item-row">
-          <label for="routing">Routing
-            <input id="routing" type="checkbox" formControlName="routing" />
-          </label>
-          <div *ngIf="this.optionsForm.invalid && (this.routing?.dirty || this.routing?.touched)" class="alert alert-danger">
-            <div *ngIf="this.routing?.errors?.['required']">
-              Routing is required.
-            </div>
-          </div>
-        </div>
-        <div class="item-row">
-          <label for="standalone">Standalone
-            <input id="standalone" type="checkbox" formControlName="standalone" />
-          </label>
-          <div *ngIf="this.optionsForm.invalid && (this.standalone?.dirty || this.standalone?.touched)" class="alert alert-danger">
-            <div *ngIf="this.standalone?.errors?.['required']">
-              Standalone is required.
-            </div>
-          </div>
-        </div>
 
-        <div class="item-row">
-          <button class="btn-primary" type="submit">Generate</button>
-        </div>
+          <div class="item-row">
+            <button class="btn-primary" type="submit">Generate</button>
+          </div>
 
+        </div>
       </div>
     </form>
     <div class="cmd-container" *ngIf="this.cmdText">
-      <div class="cmd-header">
-        <svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14">
-          <g fill="none" fill-rule="evenodd" transform="translate(1 1)">
-            <circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5"></circle>
-            <circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5"></circle>
-            <circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5"></circle>
-          </g>
-        </svg>
-        <span>
-          <span *ngIf="this.copied" class="copied">Copied!</span>
-          <button [cdkCopyToClipboard]="'ng new ' + cmdText" class="btn-copy" (click)="this.copied = true">
-            <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-14yq2cq" focusable="false" fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" data-testid="FileCopyIcon">
-              <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4 6 6v10c0 1.1-.9 2-2 2H7.99C6.89 23 6 22.1 6 21l.01-14c0-1.1.89-2 1.99-2h7zm-1 7h5.5L14 6.5V12z"></path>
-            </svg>
-          </button>
-        </span>
-      </div>
-      <div class="cmd-text">
-        <span class="command">ng new </span><span class="parameters">{{ cmdText }}</span>
+      <div class="cmd-box">
+        <div class="cmd-header">
+          <svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14">
+            <g fill="none" fill-rule="evenodd" transform="translate(1 1)">
+              <circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5"></circle>
+              <circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5"></circle>
+              <circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5"></circle>
+            </g>
+          </svg>
+          <span>
+            <span *ngIf="this.copied" class="copied">Copied!</span>
+            <button [cdkCopyToClipboard]="'ng new ' + cmdText" class="btn-copy" (click)="this.copied = true">
+              <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-14yq2cq" focusable="false" fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" data-testid="FileCopyIcon">
+                <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4 6 6v10c0 1.1-.9 2-2 2H7.99C6.89 23 6 22.1 6 21l.01-14c0-1.1.89-2 1.99-2h7zm-1 7h5.5L14 6.5V12z"></path>
+              </svg>
+            </button>
+          </span>
+        </div>
+        <div class="cmd-text">
+          <span class="command">ng new </span><span class="parameters">{{ cmdText }}</span>
+        </div>
       </div>
     </div>
     <router-outlet></router-outlet>
